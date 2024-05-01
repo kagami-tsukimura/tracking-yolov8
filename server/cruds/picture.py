d5e872976ff195652f59dd8b221e697446d032e0
Kagami <shimura.kyohei7617@gmail.com>
2024-05-01 19:06:04 +0900

    auto: auto command push

---
+++ server/cruds/picture.py
@@ -0,0 +1,30 @@
+# flake8: noqa: E402
+import os
+import sys
+from typing import List
+
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+
+parent_dir = os.path.join(os.path.dirname(__file__), "..")
+sys.path.append(parent_dir)
+
+from models import Alert
+from schemas.schemas import AlertCreate
+
+
+def create_alert(db: Session, create_alert: AlertCreate):
+    db_alert = Alert(**create_alert.model_dump())
+    db.add(db_alert)
+    db.commit()
+
+    # create_alertの前に、create_pictureの必要がある
+    # picture作成して、picture_idを返す
+    # picture_idをcreate_alertの引数に渡す
+    # camera撮影か、mp4読み込みのどちらかのステータスをcreate_alertの引数に渡す
+
+    # db_alert.picture_id でpictureテーブルから、pictureを取得
+    # {"picture": <picture>}の形式で返す
+    # ※型はstring
+
+    return {"picture_id": db_alert.picture_id}
---
+++ server/routers/picture.py
@@ -0,0 +1,26 @@
+# type: ignore
+# flake8: noqa: E402
+import os
+import sys
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException, Path, Query, Request
+from sqlalchemy.orm import Session
+from starlette import status
+
+parent_dir = os.path.join(os.path.dirname(__file__), "..")
+sys.path.append(parent_dir)
+
+from cruds import alert as alert_cruds
+from dependencies import Dependencies
+from schemas.schemas import AlertCreate, AlertResponse
+
+dependencies = Dependencies()
+DBDependency = dependencies.get_db_dependency()
+
+router = APIRouter(prefix="/alert", tags=["alert"])
+
+
+@router.post("", response_model=AlertResponse, status_code=status.HTTP_201_CREATED)
+async def create_alert(db: DBDependency, create_alert: AlertCreate):
+    return alert_cruds.create_alert(db, create_alert)
