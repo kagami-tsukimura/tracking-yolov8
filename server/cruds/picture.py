783e1f3423a87f675717c76d0faf8010640bc64b
Kagami <shimura.kyohei7617@gmail.com>
2024-05-01 20:52:39 +0900

    feat: get pic by id

--- server/cruds/picture.py
+++ server/cruds/picture.py
@@ -11,6 +11,10 @@ from models import Picture
 from schemas.schemas import PictureCreate
 
 
+def find_by_id(db: Session, picture_id: int):
+    return db.query(Picture).filter(Picture.picture_id == picture_id).first()
+
+
 def create_picture(db: Session, create_picture: PictureCreate):
     db_picture = Picture(**create_picture.model_dump())
     db.add(db_picture)
